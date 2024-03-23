d19fb88c8772efb984520bcf090e467b745ceb55
rudnmk <1132226149@pfur.ru>
2024-03-24 00:15:49 +0300

    Доделал отчет

--- 2/code/lab 2/lab 2/Calculation.cpp
+++ 2/code/lab 2/lab 2/Calculation.cpp
@@ -156,79 +156,44 @@ void calculate_density_and_acceleration(double H, double radial_vel, double tran
 	double T_acc;
 	double W_acc = 0;
 	double acc;
-	double g_120 = (6.67430 * 5.9726 * pow(10.0, 7.0)) / pow(6378.1 + 120.0, 2.0);
-	double g_500 = (6.67430 * 5.9726 * pow(10.0, 7.0)) / pow(6378.1 + 500.0, 2.0);
-	bool flag = 0;
-	double a_param_0[7];
-	double a_param_1[7];
-	double a_param_2[7];
-	double a_param_3[7];
-	double a_param_4[7];
-	double a_param_5[7];
-	double a_param_6[7];
+	double g = (6.67430 * 5.9726 * pow(10.0, 7.0)) / pow(6378.1 + H, 2.0);
+	bool lower_than_500 = 0;
+
+	double a_param_0_120[7] = { 26.8629, 27.4598, 28.6395, 29.6418, 30.1671, 29.7578, 30.7854 };
+	double a_param_1_120[7] = { -0.451674, -0.463668, -0.490987, -0.514957, -0.527837, -0.517915, -0.545695 };
+	double a_param_2_120[7] = { 0.00290397, 0.002974, 0.00320649, 0.00341926, 0.00353211, 0.00342699, 0.00370328 };
+	double a_param_3_120[7] = { -1.06953 * pow(10.0, -5.0), -1.0753 * pow(10.0, -5.0), -1.1681 * pow(10.0, -5.0), -1.25785 * pow(10.0, -5.0), -1.30227 * pow(10.0, -5.0), -1.24137 * pow(10.0, -5.0), -1.37072 * pow(10.0, -5.0) };
+	double a_param_4_120[7] = { 2.21598 * pow(10.0, -8.0), 2.17059 * pow(10.0, -8.0), 2.36847 * pow(10.0, -8.0), 2.5727 * pow(10.0, -8.0), 2.66455 * pow(10.0, -8.0), 2.48209 * pow(10.0, -8.0), 2.80614 * pow(10.0, -8.0) };
+	double a_param_5_120[7] = { -2.42941 * pow(10.0, -11.0), -2.30249 * pow(10.0, -11.0), -2.51809 * pow(10.0, -11.0), -2.75874 * pow(10.0, -11.0), -2.85432 * pow(10.0, -11.0), -2.58413 * pow(10.0, -11.0), -3.00184 * pow(10.0, -11.0) };
+	double a_param_6_120[7] = { 1.09926 * pow(10.0, -14.0), 1.00123 * pow(10.0, -14.0), 1.09536 * pow(10.0, -14.0), 1.21091 * pow(10.0, -14.0), 1.25009 * pow(10.0, -14.0), 1.09383 * pow(10.0, -14.0), 1.31142 * pow(10.0, -14.0) };
+
+	double a_param_0_500[7] = { 17.8481, -2.54909, -13.9599, -23.3079, -14.7264, -4.912, -5.40952 };
+	double a_param_1_500[7] = { -0.132025, 0.0140064, 0.0844951, 0.135141, 0.0713256, 0.0108326, 0.00550749 };
+	double a_param_2_500[7] = { 0.000227717, -0.00016946, -0.000328875, -0.000420802, -0.000228015, -8.10546 * pow(10.0, -5.0), -3.78851 * pow(10.0, -5.0) };
+	double a_param_3_500[7] = { -2.2543 * pow(10.0, -7.0), 3.27196 * pow(10.0, -7.0), 5.05918 * pow(10.0, -7.0), 5.73717 * pow(10.0, -7.0), 2.8487 * pow(10.0, -7.0), 1.15712 * pow(10.0, -7.0), 2.4808 * pow(10.0, -8.0) };
+	double a_param_4_500[7] = { 1.33574 * pow(10.0, -10.0), -2.8763 * pow(10.0, -10.0), -3.92299 * pow(10.0, -10.0), -4.03238 * pow(10.0, -10.0), -1.74383 * pow(10.0, -10.0), -8.13296 * pow(10.0, -11.0), 4.92183 * pow(10.0, -12.0) };
+	double a_param_5_500[7] = { -4.50458 * pow(10.0, -14.0), 1.22625 * pow(10.0, -13.0), 1.52279 * pow(10.0, -13.0), 1.42846 * pow(10.0, -13.0), 5.08071 * pow(10.0, -14.0), 3.04913 * pow(10.0, -14.0), -8.65011 * pow(10.0, -15.0) };
+	double a_param_6_500[7] = { 6.72086 * pow(10.0, -18.0), -2.05736 * pow(10.0, -17.0), -2.35576 * pow(10.0, -17.0), -2.01726 * pow(10.0, -17.0), -5.34955 * pow(10.0, -18.0), -4.94989 * pow(10.0, -18.0), 1.9849 * pow(10.0, -18.0) };
 
 
 	if (H < 500) {
-		data_input.open("C:/Users/mk170/MSF/2/code/lab 2/lab 2 graph/data_for_120.txt");
-		double a_param_0[7] = { 26.8629, 27.4598, 28.6395, 29.6418, 30.1671, 29.7578, 30.7854 };
-		double a_param_1[7] = { -0.451674, -0.463668, -0.490987, -0.514957, -0.527837, -0.517915, -0.545695 };
-		double a_param_2[7] = { 0.00290397, 0.002974, 0.00320649, 0.00341926, 0.00353211, 0.00342699, 0.00370328 };
-		double a_param_3[7] = { -1.06953 * pow(10.0, -5.0), -1.0753 * pow(10.0, -5.0), -1.1681 * pow(10.0, -5.0), -1.25785 * pow(10.0, -5.0), -1.30227 * pow(10.0, -5.0), -1.24137 * pow(10.0, -5.0), -1.37072 * pow(10.0, -5.0) };
-		double a_param_4[7] = { 2.21598 * pow(10.0, -8.0), 2.17059 * pow(10.0, -8.0), 2.36847 * pow(10.0, -8.0), 2.5727 * pow(10.0, -8.0), 2.66455 * pow(10.0, -8.0), 2.48209 * pow(10.0, -8.0), 2.80614 * pow(10.0, -8.0) };
-		double a_param_5[7] = { -2.42941 * pow(10.0, -11.0), -2.30249 * pow(10.0, -11.0), -2.51809 * pow(10.0, -11.0), -2.75874 * pow(10.0, -11.0), -2.85432 * pow(10.0, -11.0), -2.58413 * pow(10.0, -11.0), -3.00184 * pow(10.0, -11.0) };
-		double a_param_6[7] = { 1.09926 * pow(10.0, -14.0), 1.00123 * pow(10.0, -14.0), 1.09536 * pow(10.0, -14.0), 1.21091 * pow(10.0, -14.0), 1.25009 * pow(10.0, -14.0), 1.09383 * pow(10.0, -14.0), 1.31142 * pow(10.0, -14.0) };
-
+		lower_than_500 = 1;
 	}
 
-	else {
-		data_input.open("C:/Users/mk170/MSF/2/code/lab 2/lab 2 graph/data_for_500.txt");
-		double a_param_0[7] = { 17.8481, -2.54909, -13.9599, -23.3079, -14.7264, -4.912, -5.40952 };
-		double a_param_1[7] = { -0.132025, 0.0140064, 0.0844951, 0.135141, 0.0713256, 0.0108326, 0.00550749 };
-		double a_param_2[7] = { 0.000227717, -0.00016946, -0.000328875, -0.000420802, -0.000228015, -8.10546 * pow(10.0, -5.0), -3.78851 * pow(10.0, -5.0) };
-		double a_param_3[7] = { -2.2543 * pow(10.0, -7.0), 3.27196 * pow(10.0, -7.0), 5.05918 * pow(10.0, -7.0), 5.73717 * pow(10.0, -7.0), 2.8487 * pow(10.0, -7.0), 1.15712 * pow(10.0, -7.0), 2.4808 * pow(10.0, -8.0) };
-		double a_param_4[7] = { 1.33574 * pow(10.0, -10.0), -2.8763 * pow(10.0, -10.0), -3.92299 * pow(10.0, -10.0), -4.03238 * pow(10.0, -10.0), -1.74383 * pow(10.0, -10.0), -8.13296 * pow(10.0, -11.0), 4.92183 * pow(10.0, -12.0) };
-		double a_param_5[7] = { -4.50458 * pow(10.0, -14.0), 1.22625 * pow(10.0, -13.0), 1.52279 * pow(10.0, -13.0), 1.42846 * pow(10.0, -13.0), 5.08071 * pow(10.0, -14.0), 3.04913 * pow(10.0, -14.0), -8.65011 * pow(10.0, -15.0) };
-		double a_param_6[7] = { 6.72086 * pow(10.0, -18.0), -2.05736 * pow(10.0, -17.0), -2.35576 * pow(10.0, -17.0), -2.01726 * pow(10.0, -17.0), -5.34955 * pow(10.0, -18.0), -4.94989 * pow(10.0, -18.0), 1.9849 * pow(10.0, -18.0) };
-	}
-
+	data_input.open("C:/Users/mk170/MSF/2/code/lab 2/lab 2 graph/acceleration_data.txt");
 	for (int step = 0; step < 7; step++) {
-		density = night_density * exp(a_param_0[step] + a_param_1[step] * H + a_param_2[step] * pow(H, 2.0) + a_param_3[step] * pow(H, 3.0) + a_param_4[step] * pow(H, 4.0) + a_param_5[step] * pow(H, 5.0) + a_param_6[step] * pow(H, 6.0));
+		if (lower_than_500) {
+			density = night_density * exp(a_param_0_120[step] + a_param_1_120[step] * H + a_param_2_120[step] * pow(H, 2.0) + a_param_3_120[step] * pow(H, 3.0) + a_param_4_120[step] * pow(H, 4.0) + a_param_5_120[step] * pow(H, 5.0) + a_param_6_120[step] * pow(H, 6.0));
+		}
+		else {
+			density = night_density * exp(a_param_0_500[step] + a_param_1_500[step] * H + a_param_2_500[step] * pow(H, 2.0) + a_param_3_500[step] * pow(H, 3.0) + a_param_4_500[step] * pow(H, 4.0) + a_param_5_500[step] * pow(H, 5.0) + a_param_6_500[step] * pow(H, 6.0));
+		}
 		S_acc = (-1.0) * SIGMA * density * vel * radial_vel * 1000.0;
 		T_acc = (-1.0) * SIGMA * density * vel * transversal_vel * 1000.0;
 		acc = sqrt(pow(S_acc, 2.0) + pow(T_acc, 2.0));
 		data_input << S_acc << " " << T_acc << " " << W_acc << " " << acc << std::endl;
+		std::cout << "  g = " << g << "; overall perturbing acceleration = " << acc << std::endl;
 	}
+	data_input << lower_than_500 << std::endl;
 	data_input.close();
-
-
-
-
-	/*
-	std::cout << "-----------HEIGHT: 120 - 500 km.------------" << std::endl;
-	for (int step = 0; step < 14; step++) {
-		if (step == 7) {
-			std::cout << "----------HEIGHT: 500 - 1500 km.-------------" << std::endl;
-			data_input.close();
-			data_input.open("C:/Users/mk170/MSF/2/code/lab 2/lab 2 graph/data_for_500.txt");
-			flag = 1;
-		}
-		if (step < 7) {
-			density = night_density * exp(a_param_120_0[step] + a_param_120_1[step] * H + a_param_120_2[step] * pow(H, 2.0) + a_param_120_3[step] * pow(H, 3.0) + a_param_120_4[step] * pow(H, 4.0) + a_param_120_5[step] * pow(H, 5.0) + a_param_120_6[step] * pow(H, 6.0));
-		}
-		else {
-			density = night_density * exp(a_param_500_0[step - 7] + a_param_500_1[step - 7] * H + a_param_500_2[step - 7] * pow(H, 2.0) + a_param_500_3[step - 7] * pow(H, 3.0) + a_param_500_4[step - 7] * pow(H, 4.0) + a_param_500_5[step - 7] * pow(H, 5.0) + a_param_500_6[step - 7] * pow(H, 6.0));
-		}
-		S_acc = (-1.0) * SIGMA * density * vel * radial_vel * 1000.0;
-		T_acc = (-1.0) * SIGMA * density * vel * transversal_vel * 1000.0;
-		acc = sqrt(pow(S_acc, 2.0) + pow(T_acc, 2.0));
-		if (flag) {
-			std::cout << "  g: " << g_500 << "; overall acceleration: " << acc << std::endl;
-		}
-		else {
-			std::cout << "  g: " << g_120 << "; overall acceleration: " << acc << std::endl;
-		}
-		data_input << S_acc << " " << T_acc << " " << W_acc << " " << acc << std::endl;
-	} 
-	data_input.close();
-	*/
 }
--- 2/code/lab 2/lab 2/lab.cpp
+++ 2/code/lab 2/lab 2/lab.cpp
@@ -2,6 +2,7 @@
 
 
 int main() {
+	//можно мен€ть, что именно подаетс€ в функцию, убира€ "//" перед вызовом функции или мен€€ значени€.
 	double time;
 	double perigee_time = 0;
 	double apogee_time = T / 2.0;
